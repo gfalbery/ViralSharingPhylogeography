1ac643e2da71ac688a5699f134f7f4a6c6b62457
Greg Albery <gfalbery@gmail.com>
2019-01-17 19:10:56 -0500

    Model Files Soz

--- R Code/X_Exploring correlation among similarity matrices.R
+++ R Code/X_Exploring correlation among similarity matrices.R
@@ -1,44 +1,44 @@
-# X_ Exploring Similarity Matrix Models
+# X_ Exploring Host Similarity Matrix Models
 
-LowerHosts <- which(lower.tri(HostAdj[FHN,FHN], diag  = T))
+HostMatrixdf$PropVirus[HostMatrixdf$PropVirus==0] <- 0.0001
+HostMatrixdf$PropVirus[HostMatrixdf$PropVirus==1] <- 0.9999
 
-LongMatrixdf$PropVirus[LongMatrixdf$PropVirus==0] <- 0.0001
-LongMatrixdf$PropVirus[LongMatrixdf$PropVirus==1] <- 0.9999
-
-IM1 <- inla(data = LongMatrixdf[-HostThemselves,], 
+IM1 <- inla(data = HostMatrixdf[-HostThemselves,], 
             PropVirus ~ Space + Phylo,
             family = "beta")
 
 summary(IM1) # Interesting space is more important here ####
 
+
+
 Efxplot(list(IM1))
 
-IM1 <- inla(data = LongMatrixdf, # Doesn't fit
+IM1 <- inla(data = HostMatrixdf[-HostThemselves,], # Doesn't fit
             Virus ~ Space + Phylo,
             control.compute = list(dic = TRUE),
             family = "nbinomial")
 
-IM2 <- inla(data = LongMatrixdf, # Doesn't fit
+IM2 <- inla(data = HostMatrixdf[-HostThemselves,], # Doesn't fit
             Virus ~ Space + Phylo,
             control.compute = list(dic = TRUE),
             family = "zeroinflatednbinomial1")
 
 mf = 1
 
-MC1 <- MCMCglmm(data = LongMatrixdf, 
+MC1 <- MCMCglmm(data = HostMatrixdf, 
                 Virus ~ Space + Phylo,
                 family = "poisson",
                 nitt = 13000*mf, # REMEMBER YOU'VE DONE THIS
                 thin = 10*mf,burnin=3000*mf)
 
-MC2 <- MCMCglmm(data = LongMatrixdf, 
+MC2 <- MCMCglmm(data = HostMatrixdf, 
                 Virus ~ Space + Phylo,
                 rcov =~ idh(trait):units, 
                 family = "zipoisson",
                 nitt = 13000*mf, # REMEMBER YOU'VE DONE THIS
                 thin = 10*mf,burnin=3000*mf)
 
-MC3 <- MCMCglmm(data = LongMatrixdf, 
+MC3 <- MCMCglmm(data = HostMatrixdf, 
                 Virus ~ trait -1 + trait:(Space + Phylo),
                 rcov =~ idh(trait):units, 
                 family = "zipoisson",
@@ -47,18 +47,18 @@ MC3 <- MCMCglmm(data = LongMatrixdf,
 
 
 arrange_ggplot2(list(
-  ggplot(LongMatrixdf[-HostThemselves,], aes(Space, Phylo)) + 
+  ggplot(HostMatrixdf[-HostThemselves,], aes(Space, Phylo)) + 
     geom_point(colour = AlberColours[1], alpha = 0.3) + geom_smooth(colour = "black") +
     stat_smooth(geom = "ribbon", fill = NA, lty = 2) +
-    #coord_fixed(ratio = max(LongMatrixdf$Space)/max(LongMatrixdf$Phylo))
+    #coord_fixed(ratio = max(HostMatrixdf$Space)/max(HostMatrixdf$Phylo))
     labs(title = "Space ~ Phylogeny"), #+ ggpubr::stat_cor(method = "spearman"),
   
-  ggplot(LongMatrixdf[-HostThemselves,], aes(Space, Virus)) + 
+  ggplot(HostMatrixdf[-HostThemselves,], aes(Space, Virus)) + 
     geom_point(colour = AlberColours[2], alpha = 0.3) + geom_smooth(colour = "black") +
     stat_smooth(geom = "ribbon", fill = NA, lty = 2) +
     labs(title = "Sharing ~ Space"),# + ggpubr::stat_cor(method = "spearman"),
   
-  ggplot(LongMatrixdf[-HostThemselves,], aes(Phylo, Virus)) + 
+  ggplot(HostMatrixdf[-HostThemselves,], aes(Phylo, Virus)) + 
     geom_point(colour = AlberColours[3], alpha = 0.3) + geom_smooth(colour = "black", fill = NA) +
     stat_smooth(geom = "ribbon", fill = NA, lty = 2, col = "black") +
     labs(title = "Sharing ~ Phylogeny")# + ggpubr::stat_cor(method = "spearman")
@@ -68,12 +68,12 @@ arrange_ggplot2(list(
 
 arrange_ggplot2(list(
   
-  ggplot(LongMatrixdf[-LowerHosts,], aes(Space, PropVirus)) + 
+  ggplot(HostMatrixdf[-LowerHosts,], aes(Space, PropVirus)) + 
     geom_point(colour = AlberColours[2], alpha = 0.3) + geom_smooth(colour = "black", fill = NA) +
     stat_smooth(geom = "ribbon", fill = NA, lty = 2, col = "black") +
     labs(title = "Sharing ~ Space") + ggpubr::stat_cor(method = "spearman"),
   
-  ggplot(LongMatrixdf[-LowerHosts,], aes(Space, PropVirus2)) + 
+  ggplot(HostMatrixdf[-LowerHosts,], aes(Space, PropVirus2)) + 
     geom_point(colour = AlberColours[3], alpha = 0.3) + geom_smooth(colour = "black", fill = NA) +
     stat_smooth(geom = "ribbon", fill = NA, lty = 2, col = "black") +
     labs(title = "Sharing2 ~ Space") + ggpubr::stat_cor(method = "spearman")
@@ -83,12 +83,12 @@ arrange_ggplot2(list(
 
 arrange_ggplot2(list(
   
-  ggplot(LongMatrixdf[-HostThemselves,], aes(Space, PropVirus)) + 
+  ggplot(HostMatrixdf[-HostThemselves,], aes(Space, PropVirus)) + 
     geom_point(colour = AlberColours[2], alpha = 0.3) + geom_smooth(colour = "black", fill = NA) +
     stat_smooth(geom = "ribbon", fill = NA, lty = 2, col = "black") +
     labs(title = "Sharing ~ Space") + ggpubr::stat_cor(method = "spearman"),
   
-  ggplot(LongMatrixdf[-HostThemselves,], aes(Space, PropVirus2)) + 
+  ggplot(HostMatrixdf[-HostThemselves,], aes(Space, PropVirus2)) + 
     geom_point(colour = AlberColours[3], alpha = 0.3) + geom_smooth(colour = "black", fill = NA) +
     stat_smooth(geom = "ribbon", fill = NA, lty = 2, col = "black") +
     labs(title = "Sharing2 ~ Space") + ggpubr::stat_cor(method = "spearman")
@@ -102,7 +102,7 @@ HostThemselves <- # Removing diagonals, as they're uninformative
   which(upper.tri(HostAdj[FHN,FHN], diag = T)&lower.tri(HostAdj[FHN,FHN], diag  = T))
 
 png(filename = "Figures/Pairwise Similarity Matrix Correlations.jpg", units = "mm", width = 200, height = 200, res = 300)
-ggpairs(LongMatrixdf[-HostThemselves,], 
+ggpairs(HostMatrixdf[-HostThemselves,], 
                 lower = list(continuous = wrap("smooth", method = "gam"))) + # will take a while
   theme(strip.background = element_rect(fill = "white", colour = "dark grey")) +
   ggtitle("Space and Phylogeny Correlate with Viral Sharing")
@@ -115,7 +115,7 @@ VirusThemselves <- # Removing diagonals, as they're uninformative
           lower.tri(VirusAdj[FVN,FVN], diag  = T))
 
 png(filename = "Figures/Pairwise Virus Similarity Matrix Correlations.jpg", units = "mm", width = 200, height = 200, res = 300)
-ggpairs(VirusLongMatrixdf[-VirusThemselves,], 
+ggpairs(VirusHostMatrixdf[-VirusThemselves,], 
                 lower = list(continuous = wrap("smooth", method = "gam"))) + # will take a while
   theme(strip.background = element_rect(fill = "white", colour = "dark grey")) +
   ggtitle("Space Correlates with Host Sharing")
