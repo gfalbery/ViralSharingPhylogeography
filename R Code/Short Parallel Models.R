1e75e7d870999abaad05763b48fa6b42fddc9ae3
Gregory <gfalbery@gmail.com>
2019-01-28 15:32:54 +0000

    Checking the weekend run

---
+++ R Code/Short Parallel Models.R
@@ -0,0 +1,78 @@
+
+# Shorter parallel
+# Run source code
+
+rm(list = ls())
+
+source("R Code/00_Master Code.R")
+
+# Run parallel
+
+library(MCMCglmm); library(ggregplot); library(INLA); library(parallel); library(dplyr)
+
+UpperHosts <- # Removing diagonals and 
+  which(upper.tri(HostAdj[FHN,FHN], diag = T))
+
+FinalHostMatrix <- HostMatrixdf[-UpperHosts,]
+FinalHostMatrix$Phylo <- FinalHostMatrix$Phylo2
+FinalHostMatrix$MinDCites <- log(FinalHostMatrix$MinDCites + 1)
+FinalHostMatrixNoSpace <- FinalHostMatrix %>% filter(Space>0)
+
+gp <- gelman.prior(Virus ~ Space + Phylo2 + Space:Phylo2 + MinDCites + DomDom, data = FinalHostMatrix)
+
+prior.zi <- list(R = list(V = diag(2), nu = 0, fix = 2),
+                 G = list(G1 = list(V = diag(2), nu = 2)),
+                 B = list(V = diag(14)*10^8, mu = rep(0,14)))
+
+prior.zi$B$V[seq(2,dim(gp)[2]*2,2),seq(2,dim(gp)[2]*2,2)] <- gp
+
+prior.zi2 <- list(R = list(V = diag(2), nu = 0, fix = 2),
+                  #G = list(G1 = list(V = diag(2), nu = 2)),
+                  B = list(V = diag(14)*10^8, mu = rep(0,14)))
+
+prior.zi2$B$V[seq(2,dim(gp)[2]*2,2),seq(2,dim(gp)[2]*2,2)] <- gp
+
+# Modelling all mammal-mammal pairs ####
+
+mf = 15
+
+ZI_shortruns <- parallel::mclapply(1:10, function(i) {
+  
+  return(MCMCglmm(
+    data = FinalHostMatrix %>% filter(Space>0),
+    Virus ~ trait -1 + trait:(Space + Phylo2 + Space:Phylo2 + MinDCites + DomDom),
+    rcov =~ idh(trait):units, 
+    prior = prior.zi,
+    random =~ us(trait):mm(Sp + Sp2),
+    family = "zipoisson",
+    pr = TRUE,
+    nitt = 13000*mf, # REMEMBER YOU'VE DONE THIS
+    thin = 10*mf, burnin=8000*mf))}, mc.cores = 10)
+
+# Save file
+
+save(ZI_shortruns, file = "ZI_shortruns.Rdata")
+save.image()
+
+
+mc <- MCMCglmm(
+  data = FinalHostMatrix %>% filter(Space>0),
+  Virus ~ trait -1 + trait:(Space + Phylo2 + Space:Phylo2 + MinDCites + DomDom),
+  rcov =~ idh(trait):units, 
+  prior = prior.zi,
+  random =~ us(trait):mm(Sp + Sp2),
+  family = "zipoisson",
+  pr = TRUE,
+  nitt = 13000*mf, # REMEMBER YOU'VE DONE THIS
+  thin = 10*mf, burnin=8000*mf)
+
+mc2 <- MCMCglmm(
+  data = FinalHostMatrix %>% filter(Space>0),
+  Virus ~ trait -1 + trait:(Space + Phylo2 + Space:Phylo2 + MinDCites + DomDom),
+  rcov =~ idh(trait):units, 
+  prior = prior.zi2,
+  #random =~ us(trait):mm(Sp + Sp2),
+  family = "zipoisson",
+  pr = TRUE,
+  nitt = 13000*mf, # REMEMBER YOU'VE DONE THIS
+  thin = 10*mf, burnin=8000*mf)
