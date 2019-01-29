ca9b1df0d183ebdbc20bc54e798ad5afdcab7a71
Greg Albery <gfalbery@gmail.com>
2019-01-29 18:41:10 -0500

    Sims run

--- R Code/4_Simulating Network.R
+++ R Code/4_Simulating Network.R
@@ -1,6 +1,8 @@
 
 # Generating the network of viruses/hosts
 
+library(MCMCglmm)
+
 load("ZI_runs.Rdata")
 
 CountColumns <- list(1:7*2-1, 15:662)
@@ -16,7 +18,7 @@ ClusterMCMC <- ZI_runs[1:10 + (i-1)*10] %>% lapply(function(a) as.data.frame(as.
 
 RowsSampled <- sample(1:nrow(ClusterMCMC), 1000, replace = F)
 
-CountColumns <- list()
+logit <- function(a) exp(a)/(1 + exp(a))
 
 for(x in 1:1000){
   
@@ -27,18 +29,18 @@ for(x in 1:1000){
   CountFXSample <- ClusterMCMC[RowSampled, unlist(CountColumns)]
   ZIFXSample <- ClusterMCMC[RowSampled, unlist(ZIColumns)]
   
-  XZMatrix <- cbind(ModelList[[i]]$X, ModelList[[i]]$Z)
+  XZMatrix <- cbind(ZI_runs[[(i-1)*10+1]]$X, ZI_runs[[(i-1)*10+1]]$Z)
   
-  CountXZMatrix <- as.matrix(XZMatrix[1:N,unlist(CountColumns)])
-  ZIXZMatrix <- as.matrix(XZMatrix[(N+1):(2*N),unlist(ZIColumns)])
+  CountXZMatrix <- XZMatrix[1:N,unlist(CountColumns)] #%>% as.matrix 
+  ZIXZMatrix <- XZMatrix[(N+1):(2*N),unlist(ZIColumns)] #%>% as.matrix
   
   CountOutput <- c(CountFXSample %*% t(CountXZMatrix))
   ZIOutput <- c(ZIFXSample %*% t(ZIXZMatrix))
   
   Responses <- cbind(ZIOutput, CountOutput)
   
-  PZero <- logit(ZIOutput)
-  PCount <- exp(CountOutput)*(1-PZero)
+  PZero <- logit(ZIOutput[[1]]@x)
+  PCount <- exp(CountOutput[[1]]@x)*(1-PZero)
   
   PredList1[[x]] <- PCount
   
@@ -46,10 +48,37 @@ for(x in 1:1000){
 
 PredDF1 <- as.data.frame(PredList1)
 MeanPredictions <- apply(PredDF1,1, function(a) a %>% mean)
-
 FinalHostMatrix$PredVirus1 <- MeanPredictions
 
-ggplot(FinalHostMatrix, aes(Virus, PredVirus1)) + coord_fixed() + geom_point() + geom_smooth(method = lm)
+# Without random effects, same model ####
+
+PredList1b <- list()
+
+for(x in 1:1000){
+  
+  if(x%%10==0) print(x)
+  
+  RowSampled <- RowsSampled[x]
+  
+  CountFXSample <- ClusterMCMC[RowSampled, CountColumns[[1]]]
+  ZIFXSample <- ClusterMCMC[RowSampled, ZIColumns[[1]]]
+  
+  XZMatrix <- cbind(ZI_runs[[(i-1)*10+1]]$X, ZI_runs[[(i-1)*10+1]]$Z)
+  
+  CountXZMatrix <- XZMatrix[1:N,CountColumns[[1]]] #%>% as.matrix
+  ZIXZMatrix <- XZMatrix[(N+1):(2*N),ZIColumns[[1]]] #%>% as.matrix
+  
+  CountOutput <- c(CountFXSample %*% t(CountXZMatrix))
+  ZIOutput <- c(ZIFXSample %*% t(ZIXZMatrix))
+  
+  Responses <- cbind(ZIOutput, CountOutput)
+  
+  PZero <- logit(ZIOutput[[1]]@x)
+  PCount <- exp(CountOutput[[1]]@x)*(1-PZero)
+  
+  PredList1b[[x]] <- PCount
+  
+}
 
 # Trying it without random effects ####
 
@@ -57,30 +86,31 @@ i = 2
 
 PredList2 <- list()
 
+ClusterMCMC <- ZI_runs[1:10 + (i-1)*10] %>% lapply(function(a) as.data.frame(as.matrix(a$Sol))) %>% bind_rows %>% as.matrix
+
+RowsSampled <- sample(1:nrow(ClusterMCMC), 1000, replace = F)
+
 for(x in 1:1000){
   
-  print(x)
+  if(x%%10==0) print(x)
   
-  RowSampled <- sample(1:nrow(ModelList[[i]]$Sol), 1)
+  RowSampled <- RowsSampled[x]
   
-  CountFXSample <- ModelList[[i]]$Sol[RowSampled, CountColumns[[1]]]
-  ZIFXSample <- ModelList[[i]]$Sol[RowSampled, ZIColumns[[1]]]
+  CountFXSample <- ClusterMCMC[RowSampled, CountColumns[[1]]]
+  ZIFXSample <- ClusterMCMC[RowSampled, ZIColumns[[1]]]
   
-  XZMatrix <- cbind(ModelList[[i]]$X, ModelList[[i]]$Z)
+  XZMatrix <- cbind(ZI_runs[[(i-1)*10+1]]$X, ZI_runs[[(i-1)*10+1]]$Z)
   
-  CountXZMatrix <- as.matrix(XZMatrix[,CountColumns[[1]]])
-  ZIXZMatrix <- as.matrix(XZMatrix[,ZIColumns[[1]]])
+  CountXZMatrix <- XZMatrix[1:N,CountColumns[[1]]] #%>% as.matrix
+  ZIXZMatrix <- XZMatrix[(N+1):(2*N),ZIColumns[[1]]] #%>% as.matrix
   
   CountOutput <- c(CountFXSample %*% t(CountXZMatrix))
   ZIOutput <- c(ZIFXSample %*% t(ZIXZMatrix))
   
-  CountOutput <- CountOutput[1:N]
-  ZIOutput <- ZIOutput[(N+1):(2*N)]
-  
   Responses <- cbind(ZIOutput, CountOutput)
   
-  PZero <- logit(ZIOutput)
-  PCount <- exp(CountOutput)*(1-PZero)
+  PZero <- logit(ZIOutput[[1]]@x)
+  PCount <- exp(CountOutput[[1]]@x)*(1-PZero)
   
   PredList2[[x]] <- PCount
   
@@ -91,14 +121,13 @@ ModePredictions <- apply(PredDF2,1, function(a) a %>% mean)
 
 FinalHostMatrix$PredVirus2 <- ModePredictions
 
-ggplot(FinalHostMatrix, aes(PredVirus, Virus)) + coord_fixed() + geom_point() + geom_smooth(method = lm)
-ggplot(FinalHostMatrix, aes(PredVirus, PredVirus2)) + coord_fixed() + geom_point() + geom_smooth(method = lm)
-
 # Simulating the networks #####
 
 SimNets1 <- SimGraphs1 <- vector("list", length(PredList1))
 
 for(i in 1:length(PredList1)){
+  
+  if(i%%10==0) print(i)
   
   AssMat <- matrix(NA, 
                    nrow = length(union(FinalHostMatrix$Sp,FinalHostMatrix$Sp2)), 
@@ -116,19 +145,59 @@ for(i in 1:length(PredList1)){
   
 }
 
-Degdf <- sapply(SimGraphs1, function(a) degree(a)) %>% as.data.frame
+Degdf1 <- sapply(SimGraphs1, function(a) degree(a)) %>% as.data.frame
+Eigendf1 <- sapply(SimGraphs1, function(a) eigen_centrality(a)$vector) %>% as.data.frame
 
-Degdflong <- reshape2::melt(t(Degdf)) %>% rename(Sp = Var2, Degree = value)
+Degdflong1 <- reshape2::melt(t(Degdf1)) %>% rename(Sp = Var2, Degree = value)
 
 a = 850
 
-ggplot(Degdflong[1:a,], aes(Sp, Degree)) + geom_violin(aes(colour = Sp)) +
-  geom_point(data = Hosts[Hosts$Sp%in%Degdflong[1:a,"Sp"],], aes(Sp, Degree)) + 
+ggplot(Degdflong1[1:a,], aes(Sp, Degree)) + geom_violin(aes(colour = Sp)) +
+  geom_point(data = Hosts[Hosts$Sp%in%Degdflong1[1:a,"Sp"],], aes(Sp, Degree)) + 
   facet_wrap(~Sp, scales = "free")
 
-PredDegrees <- apply(Degdf, 1, mean)
+PredDegrees1 <- apply(Degdf1, 1, mean)
 
-Hosts$PredDegree1 <- PredDegrees[as.character(Hosts$Sp)]
+Hosts$PredDegree1 <- PredDegrees1[as.character(Hosts$Sp)]
+
+# Simulating the networks #####
+
+SimNets1b <- SimGraphs1b <- vector("list", length(PredList1b))
+
+for(i in 1:length(PredList1b)){
+  
+  if(i%%10==0) print(i)
+  
+  AssMat <- matrix(NA, 
+                   nrow = length(union(FinalHostMatrix$Sp,FinalHostMatrix$Sp2)), 
+                   ncol = length(union(FinalHostMatrix$Sp,FinalHostMatrix$Sp2)))
+  
+  AssMat[-which(1:length(AssMat)%in%UpperHosts)] <- round(PredList1b[[i]])
+  AssMat[upper.tri(AssMat)] <- t(AssMat)[!is.na(t(AssMat))]
+  diag(AssMat) <- apply(AssMat,1,function(a) length(a[!is.na(a)&a>0]))
+  dimnames(AssMat) <- list(union(FinalHostMatrix$Sp,FinalHostMatrix$Sp2),
+                           union(FinalHostMatrix$Sp,FinalHostMatrix$Sp2))
+  
+  SimNets1b[[i]] <- AssMat
+  
+  SimGraphs1b[[i]] <- graph.incidence(AssMat, weighted = TRUE)
+  
+}
+
+Degdf1b <- sapply(SimGraphs1b, function(a) degree(a)) %>% as.data.frame
+Eigendf1b <- sapply(SimGraphs1b, function(a) eigen_centrality(a)$vector) %>% as.data.frame
+
+Degdflong1b <- reshape2::melt(t(Degdf1b)) %>% rename(Sp = Var2, Degree = value)
+
+a = 850
+
+ggplot(Degdflong1b[1:a,], aes(Sp, Degree)) + geom_violin(aes(colour = Sp)) +
+  geom_point(data = Hosts[Hosts$Sp%in%Degdflong1b[1:a,"Sp"],], aes(Sp, Degree)) + 
+  facet_wrap(~Sp, scales = "free")
+
+PredDegrees1b <- apply(Degdf1b, 1, mean)
+
+Hosts$PredDegree1b <- PredDegrees1b[as.character(Hosts$Sp)]
 
 # Trying sans random effect ####
 
@@ -154,9 +223,10 @@ for(i in 1:length(PredList2)){
   
 }
 
-Degdf2 <- lapply(SimGraphs2[1:609], function(a) degree(a)) %>% as.data.frame
+Degdf2 <- sapply(SimGraphs2, function(a) degree(a)) %>% as.data.frame
+Eigendf2 <- sapply(SimGraphs2[1:729], function(a) eigen_centrality(a)$vector) %>% as.data.frame
 
-Degdflong2 <- reshape2::melt(t(Degdf)) %>% rename(Sp = Var2, Degree = value)
+Degdflong2 <- reshape2::melt(t(Degdf2)) %>% rename(Sp = Var2, Degree = value)
 
 a = 850
 
@@ -164,9 +234,23 @@ ggplot(Degdflong2[1:a,], aes(Sp, Degree)) + geom_violin(aes(colour = Sp)) +
   geom_point(data = Hosts[Hosts$Sp%in%Degdflong2[1:a,"Sp"],], aes(Sp, Degree)) + 
   facet_wrap(~Sp, scales = "free")
 
-PredDegrees <- apply(Degdf, 1, mean)
+PredDegrees2 <- apply(Degdf2, 1, mean)
 
-Hosts$PredDegree2 <- PredDegrees[as.character(Hosts$Sp)]
+Hosts$PredDegree2 <- PredDegrees2[as.character(Hosts$Sp)]
 
-ggplot(Hosts, aes(Degree, PredDegree2)) + geom_point() + geom_smooth()
+ggplot(Hosts, aes(PredDegree1b, PredDegree2)) + geom_point() + geom_smooth()
+
+GGally::ggpairs(Hosts[,c("Degree","PredDegree1","PredDegree1b","PredDegree2")],
+                lower = list(continuous = "smooth"))
+
+apply(Hosts[,c("Degree","PredDegree1","PredDegree1b","PredDegree2")],2,function(a) table(a>0))
+
+PredEigen1 <- apply(Eigendf1,1,mean)
+PredEigen1b <- apply(Eigendf1b,1,mean)
+PredEigen2 <- apply(Eigendf2,1,mean)
+
+Hosts[,c("Eigen1","Eigen1b","Eigen2")] <- cbind(PredEigen1[as.character(Hosts$Sp)],
+                                                PredEigen1b[as.character(Hosts$Sp)],
+                                                PredEigen2[as.character(Hosts$Sp)])
+
 
