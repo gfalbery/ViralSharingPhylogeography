b246960aec2c3c38687cdd52dfa7fe1885e136a9
Gregory Albery <gfalbery@gmail.com>
2019-08-10 16:43:01 -0400

    Removing more code

--- R Code/Z_Experimental Code/X_Coarse phylogenetic links in virus-host matrix.R
+++
@@ -1,9 +0,0 @@
-# Coarse phylogenetic links in virus-host matrix
-
-M %>% lapply()
-
-vFamilyList <- 
-  lapply(unique(Viruses$vFamily), function(a){ 
-    lapply(unique(Hosts$hFamily), function(b) M[Viruses$vFamily == a, Hosts$hFamily==b])})
-
-
